08b6d151949115c944d7ddfc30d8d2ce80db48a1
肖伟 <1097526615@qq.com>
2017-02-21 23:16:02 +0800

    GCD学习Demo

---
+++ GCDLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDLearn
+//
+//  Created by Metallic  on 17/2/21.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDLearn/GCDViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDLearn
+//
+//  Created by Metallic  on 17/2/21.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCDViewController : UIViewController
+
+
+@end
+
