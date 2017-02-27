cc7816fafe28fb8e4d5ad65378159ccf2d179f20
肖伟 <1097526615@qq.com>
2017-02-27 23:00:56 +0800

    串行队列和并行队列，同步和异步

--- GCDLearn/GCDViewController.m
+++ GCDLearn/GCDViewController.m
@@ -20,21 +20,55 @@ typedef void(^blk)();
 {
     [super viewDidLoad];
     
-//    dispatch_queue_t mySerialDispatchQueue1 = dispatch_queue_create("com.gcd.mySerialDispatchQueue1", NULL);
-//    dispatch_queue_t mySerialDispatchQueue2 = dispatch_queue_create("com.gcd.mySerialDispatchQueue2", NULL);
-//    
-//    dispatch_queue_t globalDispatchQueueBackground = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-//    dispatch_set_target_queue(mySerialDispatchQueue1, globalDispatchQueueBackground);
-//    
-//    dispatch_async(mySerialDispatchQueue1, ^{
-//        NSLog(@"mySerialDispatchQueue1");
-//    });
-//    dispatch_async(mySerialDispatchQueue2, ^{
-//        NSLog(@"mySerialDispatchQueue2");
-//    });
+    //创建串行队列和并行队列
+//    [self testDispatchQueueCreate];
     
-    //dispatch_queue_create
-    [self testDispatchQueueCreate];
+    //同步和异步执行操作
+//    [self testSyncAndAsync];
+    
+    //同步和异步混合
+    [self testSyncAndAsyncMix];
+}
+
+- (void)testSyncAndAsyncMix
+{
+    dispatch_queue_t serialQueue = dispatch_queue_create("com.gcd.syncAndAsyncMix.serialQueue", NULL);
+    dispatch_async(serialQueue, ^{
+        NSLog(@"1");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"11");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"111");
+    });
+    dispatch_async(serialQueue, ^{
+        NSLog(@"1111");
+    });
+    NSLog(@"2");
+    dispatch_sync(serialQueue, ^{
+        NSLog(@"3");
+    });
+    NSLog(@"4");
+}
+
+- (void)testSyncAndAsync
+{
+    //获取一个全局队列
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+
+    //要执行的block
+    void (^blk)() = ^{
+        NSLog(@"Execute block");
+    };
+
+    //同步执行操作
+    //dispatch_sync(globalQueue, blk);
+    //NSLog(@"Done!");
+        
+    //异步执行操作
+    dispatch_async(globalQueue, blk);
+    NSLog(@"Done!");
 }
 
 - (void)testDispatchQueueCreate
@@ -44,10 +78,10 @@ typedef void(^blk)();
     //优先级使用的是默认优先级，即 DISPATCH_QUEUE_PRIORITY_DEFAULT
     
     //创建串行队列
-    dispatch_queue_t mySerialQueue = dispatch_queue_create("com.gcd.queueCreate.mySerialQueue", NULL);
+//    dispatch_queue_t mySerialQueue = dispatch_queue_create("com.gcd.queueCreate.mySerialQueue", NULL);
     
     //创建并行队列
-//    dispatch_queue_t myConcurrentQueue = dispatch_queue_create("com.gcd.queueCreate.myConcurrentQueue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_queue_t myConcurrentQueue = dispatch_queue_create("com.gcd.queueCreate.myConcurrentQueue", DISPATCH_QUEUE_CONCURRENT);
     
     //执行的操作
     void (^blk1)() = ^{
@@ -69,21 +103,21 @@ typedef void(^blk)();
         NSLog(@"Execute blk6 in %@", [NSThread currentThread]);
     };
     
-    //串行队列执行三个操作，无论运行多少次顺序都是一致的
-    dispatch_async(mySerialQueue, blk1);
-    dispatch_async(mySerialQueue, blk2);
-    dispatch_async(mySerialQueue, blk3);
-    dispatch_async(mySerialQueue, blk4);
-    dispatch_async(mySerialQueue, blk5);
-    dispatch_async(mySerialQueue, blk6);
+    //串行队列执行六个操作，无论运行多少次顺序都是一致的
+//    dispatch_async(mySerialQueue, blk1);
+//    dispatch_async(mySerialQueue, blk2);
+//    dispatch_async(mySerialQueue, blk3);
+//    dispatch_async(mySerialQueue, blk4);
+//    dispatch_async(mySerialQueue, blk5);
+//    dispatch_async(mySerialQueue, blk6);
     
-    //并行队列执行三个操作，多运行几次发现每次输出顺序都可能变化，这是由iOS的XNU内核在管理线程，操作的执行可能分配到不同或相同的线程中
-//    dispatch_async(myConcurrentQueue, blk1);
-//    dispatch_async(myConcurrentQueue, blk2);
-//    dispatch_async(myConcurrentQueue, blk3);
-//    dispatch_async(myConcurrentQueue, blk4);
-//    dispatch_async(myConcurrentQueue, blk5);
-//    dispatch_async(myConcurrentQueue, blk6);
+    //并行队列执行六个操作，多运行几次发现每次输出顺序都可能变化，这是由iOS的XNU内核在管理线程，操作的执行可能分配到不同或相同的线程中
+    dispatch_async(myConcurrentQueue, blk1);
+    dispatch_async(myConcurrentQueue, blk2);
+    dispatch_async(myConcurrentQueue, blk3);
+    dispatch_async(myConcurrentQueue, blk4);
+    dispatch_async(myConcurrentQueue, blk5);
+    dispatch_async(myConcurrentQueue, blk6);
 }
 
 @end
